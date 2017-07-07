c651c52fd40ab99627e457ba8a47641ad4ca6fea
stephengeller <stephen.geller@hotmail.com>
2017-07-07 12:47:51 +0100

    Added challenge 3 using a counter and .even? method

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -7,5 +7,22 @@
 
 class String
   def every_other_char
+    arr = ''
+    counter = 1
+    each_char do |char|
+      counter += 1
+      if counter.even?
+        arr << char
+      end
+    end
+    arr
   end
 end
+
+
+# Create an empty array
+# Iterate through string
+# Count through each string number
+# Add to array if second number (a, b) or counter method
+
+"".every_other_char
