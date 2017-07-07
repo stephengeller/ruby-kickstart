d6afd9ced7eaf05c4c4e678b3b0e00fae4b0d8ad
stephengeller <stephen.geller@hotmail.com>
2017-07-07 17:54:46 +0100

    Attempted solution to challenge 4, unsuccessful on last test

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -7,19 +7,19 @@
 
 class String
   def every_other_char
-    arr = ''
-    counter = 1
-    each_char do |char|
-      counter += 1
-      if counter.even?
-        arr << char
+    str = '' # create empty string
+    counter = 1 # create a counter with odd value
+    each_char do |char| # iterate through characters in string
+      counter += 1 # add to counter to alternate between odd / even (ie a , b)
+      if counter.even? # conditional add to string
+        str << char # push character to string
       end
     end
-    arr
+    str
   end
 end
 
-
+#       Steps:
 # Create an empty array
 # Iterate through string
 # Count through each string number
