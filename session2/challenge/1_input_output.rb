92e8c77dc9b4e0adf346095f84b202e9060676cd
stephengeller <stephen.geller@hotmail.com>
2017-07-07 12:16:49 +0100

    Task 2:1 completed, peeked at solution for split.map method

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -8,5 +8,33 @@
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
+  arr = []
+  while inputnumber = gets
+    break if inputnumber == "\n"
+      inputnumber.split.map do |integer|
+      arr << integer.to_i
+      end
+   #print arr
+  end
+
+  puts arr[0] + arr[1]
+  puts arr[0] - arr[1]
+  puts arr[0] * arr[1]
+
+
+
+
   # your code goes here
+  # input1 = gets.to_i
+  # sum = input1 + input2
+  # difference = input1 - input2
+  # product = input1*input2
+  # puts sum.to_s
+  # puts difference.to_s
+  # puts product.to_s
 end
+
+
+# Use arrays, do methods
+
+#sum_difference_product
