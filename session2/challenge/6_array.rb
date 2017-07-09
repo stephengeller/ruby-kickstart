8c4fe09dc5c48e594ee4b749a8f4b4f97c7e93e6
stephengeller <stephen.geller@hotmail.com>
2017-07-09 12:32:11 +0100

    Solved challenge 7 with some borrowed from solution

--- session2/challenge/6_array.rb
+++ session2/challenge/6_array.rb
@@ -19,47 +19,47 @@
 def prime_chars?(arrayofstrings)
   length = 0
 
- # old method for getting length of string is below, before looking at solution
+ # old method for getting length of string, before looking at solution and using join and to_s methods
   # arrayofstrings.each do |string|
   #   # puts "#{string.length} is the length of the string here!"
   #   break if string.length == 0
   #   string.length > 1? length += string.length : length += 1
   # end
 
-  length = arrayofstrings.join.to_s.length
+  length = arrayofstrings.join.to_s.length # inspired off solution, but using to_s then length
 
-  2.upto length do |number|
-    if length < 2
+  2.upto length do |number| # start at 2, as 2 is a prime number
+    if length < 2 # if 1 or 0, which are not prime numbers
       return false
       break
     end
-    divnum = number + 1
-    puts "checking if #{length} is divisible by #{number}. Number var is #{number}" # test
-    if length == number
+    if length == number # if number reaches end without successful division
       return true
       break
+    elsif length % number == 0 # if number finds a smaller number to divide to
+      return false
+      break
     end
-    (length % number) == 0? (return false) : (return true)
   end
 
-  return false if arrayofstrings == [] || (length  < 2)
+  return false if arrayofstrings == [] || (length  < 2) # check to see if giving empty array or length == 1
 end
 
 
-puts "\n4\n"
-puts prime_chars? ['a', 'b', 'cd']
-puts "\n11\n"
-puts prime_chars? ['a', 'b', 'c', 'adc', 'bsd', 'c', 'd']
-puts "\nempty \n"
-puts prime_chars? []
-puts "\n''\n"
-puts prime_chars? ['']
-puts "\n'a' \n"
-puts prime_chars? ['a']
-puts "\n'ab' \n"
-puts prime_chars? ['ab']
-puts "\n'abc' \n"
-puts prime_chars? ['abc']
+# puts "\n4\n"
+# puts prime_chars? ['a', 'b', 'cd']
+# puts "\n11\n"
+# puts prime_chars? ['a', 'b', 'c', 'adc', 'bsd', 'c', 'd']
+# puts "\nempty \n"
+# puts prime_chars? []
+# puts "\n''\n"
+# puts prime_chars? ['']
+# puts "\n'a' \n"
+# puts prime_chars? ['a']
+# puts "\n'ab' \n"
+# puts prime_chars? ['ab']
+# puts "\n'abc' \n"
+puts prime_chars? ['alalalalfkfkfkfkejejejejejf']
 
 
 # divide a number by every number up until itself
--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -2,9 +2,41 @@
 # Punctuation is not part of the word unless it is a contraction.
 # In order to not have to write an actual language parser, there won't be any punctuation too complex.
 # There will be no "'" that is not part of a contraction.
-# Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
+# Assume each of these characters are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
 # 
 # Examples
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+
+
+# Take in string
+#
+
+
+def alternate_words(string)
+
+# Old attempts to remove some - but not all - punctuation from string before iterating through it
+ # substring = string.gsub(/([!@#$%^&*()-=_+|;:",.<>?])/, '').split.map.to_a
+ # substring = string.split.map.to_a
+
+
+  finalarray = []
+  '!@$#%^&*()-=_+[]:;,./<>?\\|'.split(//).each do |char| # Taken from solution due to above attempts not excluding " ' " from filter search
+    string = string.gsub(char, ' ')
+  end
+
+  string.split.each_with_index do |word, index| # with_index used instead of counter method (i += 1) to cycle through even/odd numbers
+    finalarray << word if index.even?
+  end
+  finalarray
+end
+
+
+ print alternate_words("Lo'rem ip'sum dolor sit amet.")
+ #
+ # puts alternate_words("Eleme.,.ntary, my de.,.ar Wat,.son!")
+
+print alternate_words "Would a dating service on the net be frowned upon . . . ? I hope not. But even if it is, don’t let that stop you from notifying me via net mail if you start one."
+print alternate_words "If you want to accomplish something in the world, idealism is not enough--you need to choose a method that works to achieve the goal. In other words, you need to be pragmatic." # => ["If", "want", "accomplish", "in", "world", "is", "enough", "need", "choose", "method", "works", "achieve", "goal", "other", "you", "to", "pragmatic"]
