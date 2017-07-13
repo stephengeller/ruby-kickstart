e84575474e7375b93a5672c0c8711652afe23d6a
stephengeller <stephen.geller@hotmail.com>
2017-07-13 19:45:43 +0100

    Used solution to complete challenge 2

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,27 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+
+
+def staircase(nonnegativenumber)
+  hash = {}                                       # Create empty hash
+  1.upto nonnegativenumber do |i|                 # from 1 to final number
+      next if i.even?                             # Skips even numbers
+      all = Array.new(i) {|i| i+1}                # Puts all numbers into new array, starts at 0 so need to add 1
+    evens = all.select {|e| e.even?}              # Iterates through each value, and only outputs even numbers
+    hash[i] = evens                               # Assigns the value of the key i to be the evens
+  end
+  hash                                            # Output hash
+end
+
+
+
+puts staircase 5
+
+#puts staircase 10
+
+
+# 43319 - venue ref number
+# '1530' 4353 9310 3068 8677
+
+# 846689
