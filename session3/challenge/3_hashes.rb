074d9d19b1b14bc8f1637aa213d8205bf55c4b06
stephengeller <stephen.geller@hotmail.com>
2017-07-14 13:21:54 +0100

    Completed challenge 3:3

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,20 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+
+# take in string
+# split into different words (array?)
+# iterate through the array
+# add downcased words to array
+# use counter to count number of times word has been added?
+# assign counter value as value?
+
+
+def word_count(string)
+  counts = Hash.new(0) # Create empty hash, default value 0
+  wordarr = string.split.to_a.each {|word| word.downcase!} # Split string, then convert to array, then iterate over it by making each word downcase
+  wordarr.each { |name| counts[name] += 1 } # Iterate through words,
+  counts
+end
+
