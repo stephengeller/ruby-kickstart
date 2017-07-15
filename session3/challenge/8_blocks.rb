a201e5c1e403b2b2ffc4dd12ac5b1febacbff21f
stephengeller <stephen.geller@hotmail.com>
2017-07-15 13:16:17 +0100

    Annotated challenge 3:8

--- session3/challenge/8_blocks.rb
+++ session3/challenge/8_blocks.rb
@@ -26,14 +26,39 @@
 
 
 class Person
-  attr_accessor :name
+  attr_accessor :name, :age, :quote                         # Provide getter / sett for name, age and quote
 
-  def initialize(&initializer)
-    @initializer = initializer
-    initializer.call self
+  def initialize(options=Hash.new, &initializer)            # Unlimited options (in hash) to be set, and initializer
+    self.name    = options[:name]                           # Name, age and quote set by ':option =>'
+    self.age     = options[:age]
+    self.quote   = options[:quote]
+    @initializer = (initializer || Proc.new {|person|})     # Allows for block to passed in, to set the age and quote?
+    reinit
   end
 
-  def reinit
+  def reinit                                                # Calls the block with the parameters in it, which creates the full class
     @initializer.call self
   end
 end
+
+
+artist = Person.new :name => 'Prince' do |person|
+  person.age   = 47
+  person.quote = "Why don't you purify yourself in the waters of Lake Minnetonka?"
+end
+
+puts artist.name
+
+# puts artist.name   # => "Prince"
+# puts artist.age    # => 47
+#
+# puts artist.name = 'The Artist Formerly Known As Prince'
+# puts artist.age  = 1999
+#
+# puts artist.name   # => "The Artist Formarly Known As Prince"
+# puts artist.age    # => 1999
+#
+# puts artist.reinit
+#
+# puts artist.name   # => "The Artist Formarly Known As Prince"
+# puts artist.age    # => 47
