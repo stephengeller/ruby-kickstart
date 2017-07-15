033ee30db0e17061209bb531b0d946f29546d80a
stephengeller <stephen.geller@hotmail.com>
2017-07-15 19:29:37 +0100

    Attempted 3:10, got stuck with formatting the output correctly

--- session3/challenge/10_hashes.rb
+++ session3/challenge/10_hashes.rb
@@ -29,5 +29,34 @@
 # create it from scratch :)
 
 
-def pathify
+# Iterate a hash, then iterate through the value of that hash
+
+def pathify(paths=Hash.new)
+  to_return = []
+  arr = []
+  starting_path = ["/"]
+
+  #
+  paths.each do |key, value|
+    starting_path << key
+    value.each do |key, value|
+      arr << key << value
+    end
+  end
+
+
+  to_return << arr.join('/')
+
+  final_arr = []
+  final_arr << starting_path.join << "/"  << to_return.join
+  final_arr.join
+
+  final_final_arr = []
+  final_final_arr << final_arr.join
 end
+
+p pathify 'usr' => {'bin' => ['ruby'] }                                                        # => ['/usr/bin/ruby']
+p pathify 'usr' => {'bin' => ['ruby', 'perl'] }                                                # => ['/usr/bin/ruby', '/usr/bin/perl']
+# pathify 'usr' => {'bin' => ['ruby'], 'include' => ['zlib.h'] }                               # => ['/usr/bin/ruby', '/usr/include/zlib.h']
+# pathify 'usr' => {'bin' => ['ruby']}, 'opt' => {'local' => {'bin' => ['sqlite3', 'rsync']} } # => ['/usr/bin/ruby', 'opt/local/bin/sqlite3', 'opt/local/bin/rsync']
+# pathify                                                                                      # => []
